8b001d8789d322055237f9271ba1612ddbf10783
Bruno Miguel <Brunomiguelr2023@icloud.com>
2025-10-31 19:38:03 -0300

    Add initial HTML structure for user registration

---
+++ index.js
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang>"pt-BR">
+    <head>      
+    <meta charset="UFT-8">
+    <meta name="viewport" content="width=device-width, vinitial-scale=1.0">
+    <title>Cadastro de Usuário</title></title>
+ </head>
+    <body></body>
+        <header
+            <h1>Usuário</h1>
+            <nav></nav>
+                 <ul></ul>
+                        <li><a href="index.html">Inicio</a></li>
+                        <li><a href="projeto.html">Projeto</a></li>
+                        <li><a href="cadastro.html">cadastro</a><li>
+                            <ul></ul>
+                        <nav></nav>
+
+                        <header></header>
+                        <main>
+                            <section
+                                <h2>Quem Somos</h2>
+                                <p>Somos uma organização sem fins lucrativos que atua na com animais abandonados</p>
+                                <img src="imagens/cachorro.jpg" alt="voluntários em ação"
+                            </section>
+                            
+                            </section>
+                                <h2>Missão da Ong Amigão</h2>
+                                <p>Promover dignidade e oportunidade de acolhimento aos animais abandonados</p>
+
+                            <section>
+                                <h2>Contato com ONG Amigão</h2>
+                                <address>
+                                        <p>Endereço: Rua Passagem Bingo, 123 -Bairro Bela Vista - São Paulo/SP</p>
+                                        <p>Telefone: (11) 945725576</p>
+                                        <p>Email: contato@organizaçãoongamigao.org.br</p>
+                                </address>
+
+                            </section>
+                        </main>
